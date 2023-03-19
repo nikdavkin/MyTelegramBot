2f215fa9e87c21d4f2e04649129fe6a4340886a6
Nikita <nvd2002@mail.ru>
2023-03-19 21:52:28 +0300

    Small fix

--- link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parsers/LinkParser.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parsers/LinkParser.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class LinkParser {
-    static final List<Parser> parsers = List.of(new GitHubParser(), new StackOverflowParser());
+    private static final List<Parser> parsers = List.of(new GitHubParser(), new StackOverflowParser());
 
     public static ParseResult mainParse(String URL) {
         return parsers.stream().map(p -> p.parse(URL)).filter(Objects::nonNull).findAny().orElse(null);
