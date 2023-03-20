ca5478f5a23f865a3065c7da106bcddada60f8dd
Nikita <nvd2002@mail.ru>
2023-03-20 18:56:01 +0300

    Removed main

--- link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parsers/LinkParser.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parsers/LinkParser.java
@@ -1,8 +1,6 @@
 package ru.tinkoff.edu.java.linkparser.parsers;
 
-import ru.tinkoff.edu.java.linkparser.results.GitHubResult;
 import ru.tinkoff.edu.java.linkparser.results.ParseResult;
-import ru.tinkoff.edu.java.linkparser.results.StackOverflowResult;
 
 import java.util.List;
 import java.util.Objects;
@@ -10,16 +8,7 @@ import java.util.Objects;
 public class LinkParser {
     private static final List<Parser> parsers = List.of(new GitHubParser(), new StackOverflowParser());
 
-    public static ParseResult mainParse(String URL) {
+    public ParseResult mainParse(String URL) {
         return parsers.stream().map(p -> p.parse(URL)).filter(Objects::nonNull).findAny().orElse(null);
-    }
-
-    public static void main(String[] args) {
-        String URL = "https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c";
-        ParseResult result = mainParse(URL);
-        if (result instanceof GitHubResult ghr)
-            System.out.println("GitHub result\nUser: " + ghr.user() + "\nRepo:" + ghr.repo());
-        else if (result instanceof StackOverflowResult sor) System.out.println("StackOverflow result\nID:" + sor.id());
-        else System.out.println("Wrong URL");
     }
 }
--- link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parsers/Parser.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parsers/Parser.java
@@ -2,6 +2,6 @@ package ru.tinkoff.edu.java.linkparser.parsers;
 
 import ru.tinkoff.edu.java.linkparser.results.ParseResult;
 
-interface Parser {
+public interface Parser {
     ParseResult parse(String URL);
 }
